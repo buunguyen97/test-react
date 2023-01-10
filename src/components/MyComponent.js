7f44d3e9b58a2ca470c4cb7844bedf05dfbf06b4
Buu Nguyen <buulenguyen@gmail.com>
2023-01-11 03:53:22 +0700

    video 20

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,7 +6,14 @@ class MyComponent extends Component {
     address: "HCM",
     age: 25,
   };
-  handleClick(e) {
+  handleClick = (e) => {
+    this.setState({
+      name: "niiiiii",
+    });
+    console.log("click me rooi ne");
+    console.log("My name is ", this.state.name);
+  };
+  handleHover(e) {
     console.log("SSSSSSSSSSSSSSSSSSSSSS");
     console.log(e.pageX);
   }
@@ -15,7 +22,9 @@ class MyComponent extends Component {
       <div>
         My name is {this.state.name} and I'm from {this.state.address}
         <br></br>
-        <button onMouseOver={this.handleClick}>Click me</button>
+        <button onMouseOver={this.handleHover}>Hover me</button>
+        <br></br>
+        <button onClick={this.handleClick}>Click me</button>
       </div>
     );
   }
