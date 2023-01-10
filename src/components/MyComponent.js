b670ddb29d31756c78cee6ba4bae747e1c82a1d8
Buu Nguyen <buulenguyen@gmail.com>
2023-01-11 03:29:45 +0700

    event

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,11 +6,16 @@ class MyComponent extends Component {
     address: "HCM",
     age: 25,
   };
+  handleClick(e) {
+    console.log("SSSSSSSSSSSSSSSSSSSSSS");
+    console.log(e.pageX);
+  }
   render() {
     return (
       <div>
-        My name is {this.state.name}
-        and I'm from {this.state.address}
+        My name is {this.state.name} and I'm from {this.state.address}
+        <br></br>
+        <button onMouseOver={this.handleClick}>Click me</button>
       </div>
     );
   }
